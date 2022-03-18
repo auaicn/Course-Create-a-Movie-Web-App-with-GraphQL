8e88a635073eeb9f22f7ccebccb3ca63d68b1690
Kyung Ho, Park <crorowslowpp@gmail.com>
2022-03-18 17:25:10 +0900

    Feat: fetching existing movies using @apollo/client

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,18 @@
-export default () => <div>{"Home"}</div>;
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  return <h1>home</h1>;
+};
+
+export default Home;
